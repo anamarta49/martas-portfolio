99a5366cff0281e716787d78412f8c5b0b99053f
anamarta49 <a.marta.gato@hotmail.com>
2024-06-15 20:30:35 +0100

    Adjust page transition animations

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -4,19 +4,21 @@ export const slideInAnimation = trigger('routeAnimations', [
     
     transition('* <=> *', [        
         query(':enter, :leave', 
-             style({ position: 'fixed',  width: '70vw'}), 
+             style({ position: 'absolute', width: "70vw"}), 
              { optional: true }),
+        
         group([
              query(':enter', [
-                 style({ transform: 'translateX(150%)', opacity: 0 }), 
-                 animate('0.9s ease-in-out', 
+                 style({ transform: 'translateX(100%)', opacity: 0 }), 
+                 animate('0.9s cubic-bezier(.35, 0, .25, 1)', 
                  style({ transform: 'translateX(0%)', opacity: 1}))
              ], { optional: true }),
              query(':leave', [
                  style({ transform: 'translateX(0%)'}),
-                 animate('0.8s ease-in-out', 
-                 style({ transform: 'translateX(-120%)', opacity: 0 }))
+                 animate('0.6s cubic-bezier(.35, 0, .25, 1)', 
+                 style({ transform: 'translateX(-100%)', opacity: 0 }))
                  ], { optional: true }),
          ])
    ]),
+
 ]);
