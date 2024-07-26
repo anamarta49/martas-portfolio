ed7771b903f2228a7d34a467a63422bdf6c85c16
anamarta49 <a.marta.gato@hotmail.com>
2024-07-26 11:56:21 +0100

    add animations + change styles

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { TypeWriterService } from '../shared/services/type-writer.service';
+import { map } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
   yearsOfExperience = (new Date().getFullYear()) - 2021;
+
+  titles = ['>frontend developer', '>traveller', '>artist', '>based in Lisbon'];
+
+  constructor(
+    private typeWriterService: TypeWriterService
+  ) {}
+
+  typedText = this.typeWriterService
+    .getTypewriterEffect(this.titles)
+    .pipe(map((text) => text));
 }
